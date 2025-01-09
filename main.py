596d0d34660ed8ec5bb4e909b41faf6dc233a1b7
Abhinav5551 <156273521+Abhinav5551@users.noreply.github.com>
2025-01-09 15:07:39 +0545

    updated

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageTk
 class Face_Recognition_System:
     def __init__(self, root):
         self.root = root
-        self.root.geometry("1530x790+0+0")
+        self.root.geometry("2000x1590+0+0")
         self.root.title("Face Recognition System")
 
         # First Image
